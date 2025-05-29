fbe78dc8b1626c3622d43f4b6f1a984c85c484f7
Rahil <syedrahil200@gmail.com>
2025-05-29 14:12:42 +0530

    init

---
+++ script.js
@@ -0,0 +1,17 @@
+let menu = {
+  width: 200,
+  height: 300,
+  shit: 600,
+  title: "My menu"
+};
+
+function multiplyNumeric(obj) {
+    for (const key in obj) {
+        if (typeof obj[key] === Number) {
+            obj[key] = obj[key] * 2
+            console.log(obj[key])
+        } 
+    }
+}
+multiplyNumeric(menu)
+
